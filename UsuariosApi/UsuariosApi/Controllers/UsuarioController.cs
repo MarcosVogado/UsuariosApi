beb5e6acf6c07c4a4616cfe6af73f5db37b96033
Marcos Vogado <marcoshcvogado@gmail.com>
2026-03-30 12:18:21 -0300

    feat: Setup application with init models

---
+++ UsuariosApi/UsuariosApi/Controllers/UsuarioController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class UsuarioController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CadastraUsuario(CreateUsuarioDto dto)
+    {
+        return Ok("Usuário cadastrado com sucesso!");
+    }
+}
