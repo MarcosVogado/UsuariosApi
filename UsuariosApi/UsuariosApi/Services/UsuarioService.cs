c0bc321ea8f2092749cffc8dfbadc6018aed5900
Marcos Vogado <marcoshcvogado@gmail.com>
2026-04-01 15:10:10 -0300

    feat: user login

--- UsuariosApi/UsuariosApi/Services/UsuarioService.cs
+++ UsuariosApi/UsuariosApi/Services/UsuarioService.cs
@@ -5,17 +5,19 @@ using UsuariosApi.Models;
 
 namespace UsuariosApi.Services;
 
-public class CadastroService
+public class UsuarioService
 {
     private IMapper _mapper;
     private UserManager<Usuario> _userManager;
+    private SignInManager<Usuario> _signInManager;
 
-    public CadastroService(UserManager<Usuario> userManager, IMapper mapper)
+    public UsuarioService(UserManager<Usuario> userManager, IMapper mapper, SignInManager<Usuario> signInManager)
     {
         _userManager = userManager;
         _mapper = mapper;
+        _signInManager = signInManager;
     }
-    
+
     public async Task Cadastra(CreateUsuarioDto dto)
     {
         Usuario usuario = _mapper.Map<Usuario>(dto);
@@ -25,6 +27,16 @@ public class CadastroService
         if (!resultado.Succeeded)
         {
             throw new Exception("Falha ao cadastrar usuário");
+        }
+    }
+
+    public async Task Login(LoginUsuarioDto dto)
+    {
+        var resultado = await _signInManager.PasswordSignInAsync(dto.Username, dto.Password, false, false);
+
+        if (!resultado.Succeeded)
+        {
+            throw new ApplicationException("Usuário não autenticado!");
         }
     }
 }
